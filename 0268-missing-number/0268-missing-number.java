ecfbbd02ecd8a22b699dfbe26dbb2c2737f892fb
Dilip H S <dilip_2005@outlook.com>
2026-06-23 02:56:02 +0530

    Time: 0 ms (100%), Space: 47.4 MB (31.95%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0, n = nums.length;
+        for(int i=0; i<n ;i++){
+            sum += nums[i]; 
+        }
+        int total = (n*(n+1))/2;
+        return total-sum;
+    }
+}
